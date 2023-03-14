7198e8c767038056dea65120314cdb4fd4db53f8
RakotosonFabien <rakotosonfabienmaminirina@gmail.com>
2023-03-14 20:55:29 +0300

    Création d'un bean géré par CDI (backing bean de la page customerList.xhtml)

---
+++ src/main/java/itu/mbds/fabien/tpcustomerrakotoson/jsf/CustomerBean.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package itu.mbds.fabien.tpcustomerrakotoson.jsf;
+
+import itu.mbds.fabien.tpcustomerrakotoson.ejb.CustomerManager;
+import itu.mbds.fabien.tpcustomerrakotoson.entities.Customer;
+import jakarta.ejb.EJB;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author rakot
+ */
+/**
+ * Backing bean de la page customerList.xhtml.
+ */
+@Named(value = "customerBean")  
+@ViewScoped  
+public class CustomerBean implements Serializable {  
+  private List<Customer> customerList;  
+
+  @EJB
+  private CustomerManager customerManager;  
+        
+  public CustomerBean() {  }  
+        
+  /** 
+   * Retourne la liste des clients pour affichage dans une DataTable.
+   */  
+  public List<Customer> getCustomers() {
+    if (customerList == null) {
+      customerList = customerManager.getAllCustomers();
+    }
+    return customerList;
+  }  
+}
